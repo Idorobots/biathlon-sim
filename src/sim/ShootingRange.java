50eeb0b7df3006ddfb9cf4bd9027fef53cbf80bc
Kajetan Rzepecki <kajetan.rzepecki@gmail.com>
2012-10-10 23:52:08 +0200

    Shooting Range logging
    Added some basic Shooting Range logging. Still needs some love...

--- src/sim/ShootingRange.java
+++ src/sim/ShootingRange.java
@@ -6,13 +6,20 @@ import desmoj.core.simulator.SimProcess;
 public class ShootingRange extends SimProcess {
 
 	private Biathlon myModel;
-
+    private Logger myLogger;
 
 	public ShootingRange(Model owner, String name, boolean showInTrace) {
 		super(owner, name, showInTrace);
 		myModel = (Biathlon) owner;
-	}
+        myLogger = new Logger("ShootingRange.txt");
+    }
 
+	private void log(String message) {
+        // FIXME presentTime() differs from Competitor.presentTime(), should be the same.
+
+		String msgWithTime = String.format("[ %8s ]\t%s", this.presentTime().toString(), message);
+		myLogger.log(msgWithTime);
+	}
 
 	public void lifeCycle() {
 
@@ -24,9 +31,21 @@ public class ShootingRange extends SimProcess {
 				Competitor nextCompetitor = myModel.competitorsQueue.first();
 				myModel.competitorsQueue.remove(nextCompetitor);
 
-				nextCompetitor.addPenalty(nextCompetitor.computeShotsMissed(this));
+                log(String.format("Competitor %s enters the shooting range.", nextCompetitor.toString()));
+
+                int misses = nextCompetitor.computeShotsMissed(this);
+				nextCompetitor.addPenalties(misses);
+
+                log(String.format("Competitor %s misses %d times.", nextCompetitor.toString(), misses));
+
+                // Models concrete shooting time.
 				nextCompetitor.activate(nextCompetitor.computeShootingTime());
+
+                log(String.format("Competitor %s leaves the shooting range.", nextCompetitor.toString()));
 			}
 		}
+
+//        log("Competition ends.");
+//		myLogger.close();
 	}
 }
