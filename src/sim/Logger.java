b193bec91a75270fedaf7000dc6d5dc65c91afd6
Kajetan Rzepecki <kajetan.rzepecki@gmail.com>
2012-10-17 08:58:04 +0200

    Changed javadoc for the Logger class slightly.

--- src/sim/Logger.java
+++ src/sim/Logger.java
@@ -8,11 +8,11 @@ import java.io.IOException;
 /**
  * The <code>Logger</code> class provides a convenient way of creating and
  * managing a log file.
- * 
+ *
  * @author Pawel Kleczek
  * @version 0.1
  * @since 10-10-2012
- * 
+ *
  */
 public class Logger {
 
@@ -24,9 +24,8 @@ public class Logger {
      * Creates a new <code>Logger</code> instance and associates it with a newly
      * created file specified by the given filename (the file is created in the
      * <i>log</i> directory).
-     * 
-     * @param filename
-     *            Name of the log file.
+     *
+     * @param filename Name of the log file.
      */
     public Logger(String filename) {
         logname = filename;
@@ -47,9 +46,8 @@ public class Logger {
     /**
      * Writes a string.<br>
      * Note that a newline character is added at the end of the string.
-     * 
-     * @param str
-     *            String to be written.
+     *
+     * @param str String to be written.
      */
     public void log(String str) {
 
